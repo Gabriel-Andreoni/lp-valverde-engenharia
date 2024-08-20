3eab8abed8543f2923e03ea9ea77dab2fb87540d
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-08-20 14:28:38 -0300

    Criação da faixa CTA, tornando ela já responsiva

---
+++ app/components/CTA/index.jsx
@@ -0,0 +1,14 @@
+import './cta.css';
+
+export default function CTA() {
+    return (
+        <section className="cta-container">
+            <div className="cta-content">
+                <h3>Pronto para Garantir a Segurança do Seu Estabelecimento?</h3>
+                <p>Entre em contato conosco hoje mesmo para uma avaliação técnica detalhada e dê o primeiro passo em direção a um ambiente mais seguro e conforme. </p>
+                <button>Agende uma vistoria</button>
+
+            </div>
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import CTA from "./components/CTA";
 import Hero from "./components/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
+      <CTA />
     </>
   )
 }
