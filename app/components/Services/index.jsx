29d6e6bf40c1a0fe04d40e8dd76fc2c0afb5cab4
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-08-20 15:26:02 -0300

    Criação da seção de serviços

---
+++ app/components/Services/index.jsx
@@ -0,0 +1,27 @@
+import './services.css';
+
+export default function Services() {
+    return (
+        <section className="services-container">
+            <div className="services-card">
+
+                <div className="service-content">
+                    <p>O Auto de Vistoria do Corpo de Bombeiros (AVCB) e o Certificado de Licença do Corpo de Bombeiros (CLCB) são documentos fundamentais que comprovam a adequação de um estabelecimento às normas de segurança contra incêndio estabelecidas pelo Corpo de Bombeiros.
+                    </p>
+                </div>
+                <div className="services-image">
+                </div>
+
+            </div>
+
+            <div className="services-card">
+
+                <div className="service-content">
+                    <p>A obtenção desses certificados é mais do que uma exigência legal, é uma medida de proteção à vida, ao patrimônio, e à continuidade das operações comerciais.</p>
+                </div>
+                <div className="services-image">
+                </div>
+            </div>
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import About from "./components/About";
 import CTA from "./components/CTA";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Services />
       <CTA />
     </>
   )
