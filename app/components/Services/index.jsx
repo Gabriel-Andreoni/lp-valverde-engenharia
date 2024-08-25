81eaeff8df610f47014b0f695ea432801d831a93
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-08-25 12:01:10 -0300

    Alterações na seção de serviços

--- app/components/Services/index.jsx
+++ app/components/Services/index.jsx
@@ -1,25 +1,38 @@
 import './services.css';
 
+import ArticleIcon from '@mui/icons-material/Article';
+
 export default function Services() {
     return (
         <section className="services-container">
-            <div className="services-card">
-
-                <div className="service-content">
-                    <p>O Auto de Vistoria do Corpo de Bombeiros (AVCB) e o Certificado de Licença do Corpo de Bombeiros (CLCB) são documentos fundamentais que comprovam a adequação de um estabelecimento às normas de segurança contra incêndio estabelecidas pelo Corpo de Bombeiros.
-                    </p>
-                </div>
-                <div className="services-image">
-                </div>
+            <div className="services-left">
 
             </div>
+            <div className="services-card-right">
+                <div className="services-card">
 
-            <div className="services-card">
+                    <div className="service-content">
+                        <span>
+                            <ArticleIcon />
+                        </span>
+                        <p>O Auto de Vistoria do Corpo de Bombeiros (AVCB) e o Certificado de Licença do Corpo de Bombeiros (CLCB) são documentos fundamentais que comprovam a adequação de um estabelecimento às normas de segurança contra incêndio estabelecidas pelo Corpo de Bombeiros.
+                        </p>
+                    </div>
+                    <div className="services-image">
+                    </div>
 
-                <div className="service-content">
-                    <p>A obtenção desses certificados é mais do que uma exigência legal, é uma medida de proteção à vida, ao patrimônio, e à continuidade das operações comerciais.</p>
                 </div>
-                <div className="services-image">
+
+                <div className="services-card">
+
+                    <div className="service-content">
+                        <span>
+                            <ArticleIcon />
+                        </span>
+                        <p>A obtenção desses certificados é mais do que uma exigência legal, é uma medida de proteção à vida, ao patrimônio, e à continuidade das operações comerciais.</p>
+                    </div>
+                    <div className="services-image">
+                    </div>
                 </div>
             </div>
         </section>
