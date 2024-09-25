838cefe3ca80722fa5a14bcf949366b1c2337c30
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-09-24 21:26:59 -0300

    Inserção do favicon

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ const roboto = Roboto({
 export const metadata = {
   title: "Valverde Engenharia",
   description: "Especializada em laudos AVCB e CLCB. Oferecemos soluções rápidas e eficientes para regularização e certificação de segurança contra incêndio. Confie em nossa expertise para garantir a segurança do seu empreendimento.",
+  icons: {
+    icon: "/public/favicon.png"
+  }
 };
 
 export default function RootLayout({ children }) {
